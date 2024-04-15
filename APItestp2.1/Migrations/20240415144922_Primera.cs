459409c39da407dd41b9f1ad3fbfa5ec38554163
Patricio Cainzo Pereira <pcainzo@live.com.ar>
2024-04-15 12:06:49 -0300

    Creacion de la clase usuario para Login

---
+++ APItestp2.1/Migrations/20240415144922_Primera.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APItestp2._1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Primera : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persona",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Dni = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaDeNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persona", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persona");
+        }
+    }
+}
---
+++ APItestp2.1/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace APItestp2._1.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Clave {  get; set; }
+    }
+}
